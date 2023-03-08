072c7abb0d91c5812ae11b459bddcf4800bacf96
Ignacio P <51886210+Pinto2291@users.noreply.github.com>
2023-03-07 22:55:33 -0500

    project completed sidebar

---
+++ README.md
@@ -0,0 +1,3 @@
+#Sidebar Project
+
+The objective of this project is to further developer my HTML and CSS skills
---
+++ script.js
@@ -0,0 +1,5 @@
+let hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener('click', () => {
+    document.querySelector('body').classList.toggle('active');
+})
